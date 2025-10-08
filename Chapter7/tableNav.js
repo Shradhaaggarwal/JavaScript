c60c51eaeb027dce02bdfc000e26448d0160c3f3
Shradhaaggarwal <shradhaaggarwal1402@gmail.com>
2025-10-08 21:34:12 +0530

    -Table Navigation

---
+++ Chapter7/tableNav.js
@@ -0,0 +1,11 @@
+// let t = document.body.firstElementChild.firstElementChild;
+let t = document.body.firstElementChild.firstElementChild;
+console.log(t);
+
+console.log(t.rows);
+console.log(t.caption); 
+console.log(t.tHead);
+console.log(t.tFoot);
+console.log(t.tBodies);
+console.log(t.rows[1].cells);
+console.log(t.rows[1].cells[0].innerText);

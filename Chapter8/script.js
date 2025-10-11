ca062dc119a0bd6250f0c693f82d414fbd41c49b
Shradhaaggarwal <shradhaaggarwal1402@gmail.com>
2025-10-11 21:09:47 +0530

    -set interval, set time out -Browser events -Addeventlistener, removeeventlistener -Practice set 8

--- Chapter8/script.js
+++ Chapter8/script.js
@@ -84,3 +84,101 @@ console.log(id.classList.contains('red'));
 //.remove
 id.className = "yellow tcolor"
 console.log(id.classList);
+
+
+//settimeout
+// let q = setTimeout(function(){
+//     alert("hello");
+// }, 2000);
+// console.log(q);
+
+
+
+//with a function
+// let sum = (a,b) =>{
+//     console.log("running"+ a+b);
+// }
+// setTimeout(sum, 5000, 1, 5);
+
+
+
+//set interval
+// setInterval(function(){
+//     alert("hi")
+// }, 7000)
+// clearInterval();
+
+//EVENT HANDLERS
+let v = document.getElementsByClassName('but')[0];
+v.onclick = () =>{
+    let y = document.getElementsByClassName('but')[0];
+    y.innerHTML = 'im clicked';
+}
+//js wala onclick mana jayega in case of conflict
+
+
+//addEventListener and remove event listener
+let but1 = document.getElementById("but1");
+let fun1 = function(e){
+    alert("hello im fun1");
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(e.type);
+    // console.log(e.clientX, e.clientY);
+}
+let fun2 = function(e){
+    alert("hello im fun2");
+}
+
+let clicked = function(e){
+    let xx = prompt("enter a numeber: ");
+    if(xx==1){
+    but1.removeEventListener('click',fun1);
+}
+}
+
+but1.addEventListener('click', clicked);
+but1.addEventListener('click', fun1);
+but1.addEventListener('click', fun2);
+//e = event object
+
+
+//PRACTICE SET 8
+//1. create diff alerts when diff buttons are clicked
+
+let psb1e = function(e){
+    alert("psb1 is clicked");
+}
+
+let psb2e = function(e){
+    alert("psb2 is clicked");
+}
+
+psb1.addEventListener('click', psb1e);
+psb2.addEventListener('click', psb2e);
+
+//2. buttons to reach diff websites with diff buttons
+//direct anchor tag use krlo <a href="google.com"><button>google</button></a>
+//OR
+document.getElementById('google').addEventListener('click',function(e){
+    window.location = "https://google.com";
+})
+
+//3. write a js program to keep fetching data from a website every 5 sec.
+//later
+
+//4. Create a bulb using toggle method
+let bulb = document.getElementById('bulb');
+let even = function(e){
+    bulb.classList.toggle("bulbon");
+    if (bulb.classList.contains('bulbon')) {
+        bulb.classList.remove('bulboff');
+        bulb.textContent = 'on';
+    } else {
+        bulb.classList.add('bulboff');
+        bulb.textContent = 'off';
+    }
+}
+
+
+bulb.addEventListener('click', even);

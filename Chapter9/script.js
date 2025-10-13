5ab732ede7060e1a09422b53c055fce3c40ab6b4
Shradhaaggarwal <shradhaaggarwal1402@gmail.com>
2025-10-13 16:55:55 +0530

    -Async await -try catch -error obj and custom error -Finally -Practice set 9

--- Chapter9/script.js
+++ Chapter9/script.js
@@ -90,3 +90,147 @@ pall.then((value)=>{
     console.log("resolved" + value);
 })
 
+
+//Async and await
+//async makes a func return a promise and await makes a func wait for a promise
+
+async function wreport(){
+    let prom = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("learning async and await")
+            resolve("resolved")
+        }, 11000);
+    })
+
+    let prom2 = new Promise((resolve, error)=>{
+        setTimeout(()=>{
+            console.log("learning asyncs and await 2")
+            resolve("resolved2")
+        }, 10000)
+    })
+    // let a = await prom;
+    // let b = await prom2;
+    let a = Promise.all([prom, prom2])
+    // console.log(a + b);
+    return a;
+}
+
+let report = wreport();
+console.log(report);
+
+wreport().then((value)=>{
+    console.log("returned:" + value);
+})
+
+
+//ERROR handleing , try catch 
+try{
+    console.log(shradha);
+}
+catch{
+    console.log("its okay, it happens");
+} //works synchronously and doesnt work for scheduled code
+
+
+
+//Error object and custom error
+try{
+    // console.log(shradha);
+    // throw new Error('invalid');
+    throw new SyntaxError('sorry');
+} catch(error){
+    console.log("its okay happens");
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+}
+
+//Finally clause - runs in any case
+try{
+    console.log(p);
+} catch(err){
+    console.log("its okay");
+    console.log(x);
+}finally{
+    console.log("BHAI MAI TOH CHALUNGA HE CHALUNGA");
+}
+
+
+
+//PRACTICE SET 9
+//1. write a program in js to load a file in a browser using promises and send an alert when loaded
+
+const loadS = async (src)=>{
+    let psp =  new Promise((resolve, reject)=>{
+        let sc = document.createElement("script");
+        sc.src = src;
+        sc.onload = () => resolve(src);
+        sc.onerror = () => reject( new Error (`Failed to load: ${src}`));
+        document.head.append(sc);
+        // console.log(sc);
+    })
+    return psp.then(()=>{
+        console.log("hurray");
+    }, (Error)=>{
+        console.log("shit");
+    })   
+}
+
+
+const main2 = async () => {
+    let a = await loadS("script1.js");
+    console.log(a);
+}
+main2();
+
+//2. with async await
+//in first only both .then and 2.
+
+//3. create a promise which rejects after 3 secs, use async and await to get a value and use try catch to handle a value.
+
+const qs3 = async () =>{
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            reject(new Error("sorryyyyy!!!"))
+        }, 3000)
+    })
+}
+
+
+try{
+    let qs3aw = await qs3();
+    console.log(qs3aw);
+} catch(err){
+    console.log("sorry theres an error");
+}
+
+//4. write a func async await with promise.all inside to await three promises AND compare the result when we wait for each promise one by one
+
+let qs4 = async () =>{
+    let qp1 = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            // console.log("qps1");
+            resolve("qps1");
+        },3000)
+    })
+
+    let qp2 = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            // console.log("qps2");
+            resolve("qps2");
+        },4000)
+    })
+
+    let qp3 = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            // console.log("qps3");
+            resolve("qps3");
+        },5000)
+    })
+
+    let proall = await Promise.all([qp1, qp2, qp3]);
+    console.log(proall);
+
+}
+
+qs4();
